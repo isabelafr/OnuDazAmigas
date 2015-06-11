21013084847a4cdc923ba77fc6ee52094ec04668
Isabela Ribeiro <bellrribeiro@gmail.com>
2015-06-11 20:03:55 -0300

    Descrição do Aplicativo

---
+++ descrição.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 19:57:01 2015
+
+@author: Isabela
+"""
+
+"""
+1- 	Nome do projeto: Onu Dazamigas
+2-	Integrantes: Bruna Di Bisceglie, Daniela Bento, Fernanda Dib e Isabela Ribeiro.
+3-	Potencial usuário do projeto: adolescentes e adultos
+4-	Descrição do produto: o app tem por objetivo formar uma comissão de amigas que vão votar se uma amiga pode ou não mandar certa mensagem para uma pessoa problema.
+5-	Não existem programas e aplicativos similares
+6-	Recursos utilizados: tutorias online, livros, YouTube, pesquisas no Google, funções, listas e loops do Python, Kivy e vários outros.
+7-	Planos B: 
+"""
